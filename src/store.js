c8bc4aece72d622b9ab9e5e901e7316e37c30431
zidnafadla <zidna.me@gmail.com>
2021-01-02 17:15:54 +0700

    test store

--- src/store.js
+++ src/store.js
@@ -1,14 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
+var baseAPI = 'http://localhost:8080/';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    is_loading: false,
+    token: localStorage.getItem('_gsid') || null,
+    ujianList: []
   },
   mutations: {
+    setListUjian(state, data) {
+      state.ujianList = data;
+    },
+    loading(state, status = true) {
+      state.is_loading = status;
+    }
   },
   actions: {
+    getUjian({ state, dispatch }) {
+      dispatch('loading', true);
+      return new Promise((resolve, reject){
+        axios
+          .post(baseAPI + '/api/ujian-list?' + state.token)
+          .then((ress) => {
+            dispatch('loading', false);
+            if (ress.status == 'OK') {
+              dispatch('setListUjian', ress.data.list);
+            }
+            resolve(ress)
+          })
+          .catch((err) => {
+            dispatch('loading', false);
+            reject(err)
+          })
+      })
+    }
   },
   modules: {
   }
